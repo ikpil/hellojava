2b74599d7708cc37940db96510816d8124628919
ikpil <ikpil.choi@supercat.co.kr>
2019-07-18 14:36:10 +0900

    CopyOnWirteArraySet 예제 추가

--- src/main/java/com/ikpil/hello/java/ConsoleCommand.java
+++ src/main/java/com/ikpil/hello/java/ConsoleCommand.java
@@ -1,8 +1,13 @@
 package com.ikpil.hello.java;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.ArrayList;
 
 public class ConsoleCommand {
+    private static final Logger logger = LoggerFactory.getLogger(ConsoleCommand.class);
+
     public static void main(String[] args) {
         ArrayList<Example> examples = new ArrayList<>();
         examples.add(new CollectionExample());
