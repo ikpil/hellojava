61185eb8d24d7d60da1e068b37ff4f9375d23a09
ikpil <ikpil@naver.com>
2020-01-15 09:45:41 +0900

    added - 스프링 부트 레이어링 테스트

---
+++ hj-ffm-spring-layering/src/main/java/com/ikpil/hello/java/spring/layering/App.java
@@ -0,0 +1,11 @@
+package com.ikpil.hello.java.spring.layering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
