bfd39a358213599f03b4d5a2c02447c6c5721f88
ikpil <ikpil@naver.com>
2020-02-19 09:26:04 +0900

    tictactoe 추가

---
+++ hj-react/src/TicTacToe.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import Table from './Table';
+
+const TicTacToe = () => {
+    const [winner, setWinner] = useState('');
+    const [turn, setTurn] = useState('O');
+    const [tabledata, setTableData] = useState([['', '', ''], ['', '', ''], ['', '', '']])
+
+    return (
+        <>
+            <Table />
+            {winner && <div>{winner}님의 승리</div>}
+        </>
+    );
+}
+
+export default TicTacToe;
---
+++ hj-react/src/Tr.jsx
@@ -0,0 +1,10 @@
+import React, { memo, useContext } from 'react';
+import Td from './Td';
+
+const Tr = () => {
+    return (
+        <Td>{''}</Td>
+    );
+}
+
+export default Tr;
