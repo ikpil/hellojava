9b428004a1999b769cd84fd7e6b394e7e4ffa5db
ikpil <ikpil@naver.com>
2020-02-20 09:55:51 +0900

    added - 쿼리에 파라미터 넣고 정상 출력되는지 체크

--- hj-ffm-spring-layering/src/main/java/com/ikpil/hello/java/spring/layering/App.java
+++ hj-ffm-spring-layering/src/main/java/com/ikpil/hello/java/spring/layering/App.java
@@ -29,8 +29,11 @@ public class App implements CommandLineRunner {
 
     @Override
     public void run(String... strings) throws Exception {
-        String sql = "SELECT 1";
-        var param = new MapSqlParameterSource();
+        String sql = "SELECT :a + :b";
+        var param = new MapSqlParameterSource()
+                .addValue("a", 100)
+                .addValue("b", 200);
+
         Integer result = jdbcTemplate.queryForObject(sql, param, Integer.class);
 
         System.out.println("result = " + result);
