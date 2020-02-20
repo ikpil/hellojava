d45c37c776cd9b91515e418e840e88afc9b09b97
ikpil <ikpil@naver.com>
2020-02-20 09:51:07 +0900

    added - JdbcTemplate 로 DB 접속 테스트

--- hj-ffm-spring-layering/src/main/java/com/ikpil/hello/java/spring/layering/App.java
+++ hj-ffm-spring-layering/src/main/java/com/ikpil/hello/java/spring/layering/App.java
@@ -7,6 +7,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 @SpringBootApplication
 @ComponentScan
@@ -14,13 +16,24 @@ public class App implements CommandLineRunner {
     @Autowired
     CustomerService customerService;
 
-    @Override
-    public void run(String... strings) throws Exception {
+    @Autowired
+    NamedParameterJdbcTemplate jdbcTemplate;
+
+    public void run_bak(String... strings) throws Exception {
         customerService.save(new Customer(1, "Nobita", "Nobi"));
         customerService.save(new Customer(2, "Takeshi", "Goda"));
         customerService.save(new Customer(3, "Suneo", "Honekawa"));
 
         customerService.findAll().forEach(System.out::println);
+    }
+
+    @Override
+    public void run(String... strings) throws Exception {
+        String sql = "SELECT 1";
+        var param = new MapSqlParameterSource();
+        Integer result = jdbcTemplate.queryForObject(sql, param, Integer.class);
+
+        System.out.println("result = " + result);
     }
 
     public static void main(String[] args) {
