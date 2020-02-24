371708741b6f524f5e3988f472f6b63cd45dca16
ikpil <ikpil@naver.com>
2020-02-25 08:34:19 +0900

    프로젝트 이동

--- hj-react/src/client.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-import { hot } from 'react-hot-loader/root';
-import TicTacToe from './TicTacToe';
-
-const HotLoader = hot(TicTacToe);
-
-ReactDom.render(<HotLoader />, document.querySelector('#root'));
