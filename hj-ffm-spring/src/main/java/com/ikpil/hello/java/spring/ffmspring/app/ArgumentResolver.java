3e47e31ba345800a5d3372f32a04628df314a109
ikpil <ikpil@naver.com>
2020-02-29 02:53:01 +0900

    removed - 저장소 이동에 따른 삭제

--- hj-ffm-spring/src/main/java/com/ikpil/hello/java/spring/ffmspring/app/ArgumentResolver.java
+++
@@ -1,7 +0,0 @@
-package com.ikpil.hello.java.spring.ffmspring.app;
-
-import java.io.InputStream;
-
-public interface ArgumentResolver {
-    Argument resolve(InputStream stream);
-}
--- hj-ffm-spring/src/main/java/com/ikpil/hello/java/spring/ffmspring/app/Calculator.java
+++
@@ -1,5 +0,0 @@
-package com.ikpil.hello.java.spring.ffmspring.app;
-
-public interface Calculator {
-    int calc(int a, int b);
-}
