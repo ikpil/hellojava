994b3d262afb15e18ec11df682dfc564caf65970
ikpil <ikpil@naver.com>
2020-02-28 10:46:16 +0900

    added - NamedParamterJdbcTempalte 을 사용해서 Repository 구현

--- hj-ffm-spring-layering/src/main/java/com/ikpil/hello/java/spring/layering/App.java
+++ hj-ffm-spring-layering/src/main/java/com/ikpil/hello/java/spring/layering/App.java
@@ -1,59 +1,28 @@
 package com.ikpil.hello.java.spring.layering;
 
 import com.ikpil.hello.java.spring.layering.domain.Customer;
-import com.ikpil.hello.java.spring.layering.service.CustomerService;
+import com.ikpil.hello.java.spring.layering.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 @SpringBootApplication
 @ComponentScan
 public class App implements CommandLineRunner {
     @Autowired
-    CustomerService customerService;
-
-    @Autowired
-    NamedParameterJdbcTemplate jdbcTemplate;
+    CustomerRepository repository;
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
 
-    public void run_step1(String... strings) throws Exception {
-        customerService.save(new Customer(1, "Nobita", "Nobi"));
-        customerService.save(new Customer(2, "Takeshi", "Goda"));
-        customerService.save(new Customer(3, "Suneo", "Honekawa"));
-
-        customerService.findAll().forEach(System.out::println);
-    }
-
-    public void run_step2(String... strings) throws Exception {
-        String sql = "SELECT :a + :b";
-        var param = new MapSqlParameterSource()
-                .addValue("a", 100)
-                .addValue("b", 200);
-
-        Integer result = jdbcTemplate.queryForObject(sql, param, Integer.class);
-
-        System.out.println("result = " + result);
-    }
-
     @Override
     public void run(String... strings) throws Exception {
-        String sql = "SELECT * FROM `customers` WHERE `id` = :id";
-        var param = new MapSqlParameterSource()
-                .addValue("id", 1);
+        var created = repository.save(new Customer(null, "밀란", "쿤데라"));
+        System.out.println(created + " is created!");
 
-        var result = jdbcTemplate.queryForObject(sql, param, (rs, num) -> new Customer(
-                rs.getInt("id"),
-                rs.getString("first_name"),
-                rs.getString("last_name")
-        ));
-
-        System.out.println("result = " + result);
+        repository.findAll().forEach(System.out::println);
     }
 }
--- hj-ffm-spring-layering/src/main/java/com/ikpil/hello/java/spring/layering/repository/CustomerRepository.java
+++ hj-ffm-spring-layering/src/main/java/com/ikpil/hello/java/spring/layering/repository/CustomerRepository.java
@@ -1,30 +1,50 @@
 package com.ikpil.hello.java.spring.layering.repository;
 
 import com.ikpil.hello.java.spring.layering.domain.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
+import java.util.Objects;
 
 @Repository
+@Transactional // 각 메서드를 다른 클래스에서 호출하면 DB 트랜잭션이 자동으로 이루어짐
 public class CustomerRepository {
-    private final ConcurrentMap<Integer, Customer> customerMap = new ConcurrentHashMap<>();
+    private static final RowMapper<Customer> customerRowMapper = (rs, num) -> new Customer(
+            rs.getInt("id"),
+            rs.getString("first_name"),
+            rs.getString("last_name")
+    );
+    @Autowired
+    NamedParameterJdbcTemplate jdbcTemplate;
 
     public List<Customer> findAll() {
-        return new ArrayList<>(customerMap.values());
+        return jdbcTemplate.query("SELECT * FROM `customers` ORDER BY `id`", customerRowMapper);
     }
 
-    public Customer findOne(Integer customerId) {
-        return customerMap.get(customerId);
+    public Customer findOne(Integer id) {
+        var param = new MapSqlParameterSource().addValue("id", id);
+        return jdbcTemplate.queryForObject("SELECT * FROM `customers` WHERE `id` = :id", param, customerRowMapper);
     }
 
     public Customer save(Customer customer) {
-        return customerMap.put(customer.getId(), customer);
+        var param = new BeanPropertySqlParameterSource(customer);
+        if (Objects.isNull(customer.getId())) {
+            jdbcTemplate.update("INSERT INTO `customers` (`first_name`, `last_name`) VALUES (:firstName, :lastName)", param);
+        } else {
+            jdbcTemplate.update("UPDATE `customers` SET `first_name` = :firstName, `last_name` = :lastName WHERE `id` = :id", param);
+        }
+
+        return customer;
     }
 
     public void delete(Integer customerId) {
-        customerMap.remove(customerId);
+        var param = new MapSqlParameterSource().addValue("id", customerId);
+        jdbcTemplate.update("DELETE FROM `customers` WHERE `id` = :id", param);
     }
 }
