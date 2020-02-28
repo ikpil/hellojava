b899c4af93626a5cbf87038246cd12b653a47dff
ikpil <ikpil@naver.com>
2020-02-28 11:12:18 +0900

    removed - 다른 저장소로 이동에 따른 싹제

--- hj-ffm-spring-layering/src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `customers` (`first_name`, `last_name`) VALUES('재롱', '김');
-INSERT INTO `customers` (`first_name`, `last_name`) VALUES('우주', '코스모스');
-INSERT INTO `customers` (`first_name`, `last_name`) VALUES('복남', '박');
-INSERT INTO `customers` (`first_name`, `last_name`) VALUES('익필', '최');
-
